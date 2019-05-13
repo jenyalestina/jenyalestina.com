f1f1fe735dc2254ce149c7efccf48493fee00bae
Jenya <jenya@jenyalestina.com>
2019-05-12 20:47:50 -0700

    Changed copy on homepage

--- index.php
+++ index.php
@@ -9,19 +9,19 @@
         <?php include 'z-top.php';?>
         <div class="wrapper">
         <?php include 'z-navmain.php';?>
-        
+
         		<img id="selfportrait" src="temp-profile.jpg">
         		<h1>About Me</h1>
-        		<p>Hello world! My name is Jenya Lestina. I’m a programmer in HTML, CSS, JavaScript, Java, 
-        		PHP, SQL, and Python. I'm also an artist and a writer, and a young person completing an 
-        		Associate's degree in computer science. I’m presently looking for full-time entry-level 
-        		work as a programmer for a technology-focused business.</p>
-        		<p>For more info about that, please check out the <a href="hireme/hireme.php">Hire Me</a> link, 
-        		where you’ll find additional information about my resume, skills, and credentials, as well 
+        		<p>Hello world! My name is Jenya Lestina. I’m a digital marketer and a programmer in Python, Golang,
+              SQL, HTML, CSS, JavaScript, PHP, and Java. I'm also an artist and a writer. I currently
+              work for <a href="https://www.upgrow.io">Upgrow</a>, a marketing firm based in San Francisco,
+              California. I'd like for my next job to be a programmer in any of the above langugages.</p>
+        		<p>For more info about that, please check out the <a href="hireme/hireme.php">Hire Me</a> link,
+        		where you’ll find additional information about my resume, skills, and credentials, as well
         		as examples of my previous work.</p>
-        		<p>I'm also available for freelance writing and art commissions. If you're interested, 
+        		<p>I'm also available for freelance writing and art commissions. If you're interested,
         		check out the <a href="commission.php">Commission Me</a> link for details!</p>
-        		<p>Finally, if you'd like to reach out to me, check out the <a href="contact.php">Contact Me</a> 
+        		<p>Finally, if you'd like to reach out to me, check out the <a href="contact.php">Contact Me</a>
         		link for my contact info, plus social media!</p>
         </div>
         <script src="script.js"></script>
